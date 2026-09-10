59327998baf8f80ef1330199767bfe8020d51314
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-09-10 10:37:54 +0200

    Created the add item endpoint using the POST method to create a new item

---
+++ src/routes/itemsRoute.ts
@@ -0,0 +1,22 @@
+import {IncomingMessage, ServerResponse} from 'http'
+import {addItem, getItems, getItemById, updateItem, deleteItem} from '../controllers/itemController.js'
+
+export const itemsRoute = async (req: IncomingMessage, res: ServerResponse) => {
+    if(req.url?.startsWith('/items')){
+        console.log(req.url, 'request url'); //so we can see what the request url looks like
+
+        //splitting the request url into parts(split after every "/")
+        const parts = req.url.split("/") //returns an array in which every part/piece of the url is stored
+        console.log(parts, 'url parts')
+
+        const id = parts[2] ? parseInt(parts[2]) : undefined //convert id from string to integer if id is found
+
+        //adding an item
+        if (req.method === 'POST'){
+            let body = ""
+            req.on("data", (chunk) => { //chunk contains the piece of data that just arrived; .on is the standard way of listening for events on an object; data is the specific event that is being listened for
+                body += chunk.toString(); //we convert chunk from a buffer(raw binary data) to a string
+            })
+        }
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import http, {IncomingMessage, ServerResponse} from "http"
+import {itemsRoute} from './routes/itemsRoute.js'
 
-const PORT = 4000
-const requestListener = (req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, {"content-type": "application/json"})
-    res.end(JSON.stringify({message: "Creating server"})) //res.end closes the connection
+const PORT = 4002
+const requestListener = async (req: IncomingMessage, res: ServerResponse) => {
+    await itemsRoute(req, res);
+    // res.writeHead(200, {"content-type": "application/json"})
+    // res.end(JSON.stringify({message: "Creating server"})) //res.end closes the connection
 }
 
 const server = http.createServer(requestListener) //we create a new server instance and pass our function
